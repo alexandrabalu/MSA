bab1558d68b7f9b1a0e718352cd7d1b6d75a806d
balu_alexandra <balu_alexandra@yahoo.com>
2019-10-10 15:09:37 +0300

    App layout + game architecture

---
+++ app/src/main/java/com/example/alexandra/a2048game/Game.java
@@ -0,0 +1,16 @@
+package com.example.alexandra.a2048game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class Game extends AppCompatActivity
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("ANA are mere");
+    }
+}
--- app/src/main/java/com/example/alexandra/a2048game/MainActivity.java
+++ app/src/main/java/com/example/alexandra/a2048game/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.alexandra.a2048game;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View v)
+    {
+        Intent i = new Intent(this, Game.class);
+        startActivity(i);
     }
 }
