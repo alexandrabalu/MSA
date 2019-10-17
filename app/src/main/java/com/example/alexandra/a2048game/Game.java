37ef82edc62b1da6b164dbfbebc6864d7d45796a
balu_alexandra <balu_alexandra@yahoo.com>
2019-10-17 15:58:07 +0300

    Game Layout. added activity_game

--- app/src/main/java/com/example/alexandra/a2048game/Game.java
+++ app/src/main/java/com/example/alexandra/a2048game/Game.java
@@ -10,7 +10,7 @@ public class Game extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("ANA are mere");
+        setContentView(R.layout.activity_game);
+
     }
 }
