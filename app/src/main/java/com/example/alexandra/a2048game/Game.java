c530363365fb9c5db1962cad3aa9ac5231a82f3a
balu_alexandra <balu_alexandra@yahoo.com>
2019-11-14 16:05:41 +0200

    Right Move

--- app/src/main/java/com/example/alexandra/a2048game/Game.java
+++ app/src/main/java/com/example/alexandra/a2048game/Game.java
@@ -24,7 +24,7 @@ public class Game extends AppCompatActivity
     TextView textView = null;
     int score = 0;
     int side = 4;
-    int[][] tiles = {{0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}} ;
+    Integer[] titles = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     Random rand = new Random();
 
 
@@ -36,25 +36,174 @@ public class Game extends AppCompatActivity
         textView = (TextView) findViewById(R.id.Score);
         grid = (GridView) findViewById(R.id.gridView);
 
-        GameAdapter gameAdapter = new GameAdapter(this,tiles);
-        grid.setAdapter(gameAdapter);
+        int pos = pickRandom(titles);
+        titles[pos] = 2;
+
+        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles);
+        grid.setAdapter(arrayAdapter);
+
+        grid.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(keyEvent.getAction() == KeyEvent.ACTION_DOWN )
+                {
+                    if( i == KeyEvent.KEYCODE_DPAD_RIGHT)
+                    {
+                        titles = rightMove(titles);
+                        int pos = pickRandom(titles);
+                        titles[pos] = 2;
+                        changeGrid(titles);
+                    }
+
+                    if( i == KeyEvent.KEYCODE_DPAD_LEFT)
+                    {
+                        int pos = pickRandom(titles);
+                        titles[pos] = 2;
+                        changeGrid(titles);
+                    }
+
+                    if( i == KeyEvent.KEYCODE_DPAD_UP)
+                    {
+                        int pos = pickRandom(titles);
+                        titles[pos] = 2;
+                        changeGrid(titles);
+                    }
+
+                    if( i == KeyEvent.KEYCODE_DPAD_DOWN)
+                    {
+                        int pos = pickRandom(titles);
+                        titles[pos] = 2;
+                        changeGrid(titles);
+                    }
+                }
+                return false;
+            }
+        });
 
     }
 
-    private  void addRandomTile()
+    private int pickRandom(Integer[] array)
     {
-        int pos = rand.nextInt(side * side);
-        int row, col;
-        do {
-            pos = (pos + 1) % (side * side);
-            row = pos / side;
-            col = pos % side;
-        } while (tiles[row][col] != 0);
+        Random rand = new Random();
+        int pos = -1;
+        while(true)
+        {
+            pos = rand.nextInt(16);
+            if(array[pos] == 0)
+            {
+                return pos;
+            }
+        }
 
-        int val = rand.nextInt(10) == 0 ? 4 : 2;
-        tiles[row][col] = val;
+
+    }
+    private int[][] convertArray(Integer[] array)
+    {
+        int side = 4 ;
+        int[][] matrix = new int[4][4];
+
+        int count = 0;
+
+        for(int i = 0; i < side; i++)
+        {
+            for(int j = 0 ; j < side ; j++)
+            {
+                matrix[i][j] = array[count];
+                count++;
+            }
+        }
+
+        return matrix;
+
     }
 
+    private Integer[] convertMatrix(int[][] matrix)
+    {
+        int side = 4 ;
+        Integer[] array = new Integer[side*side];
+
+        int count = 0;
+
+        for(int i = 0; i < side; i++)
+        {
+            for(int j = 0 ; j < side ; j++)
+            {
+                 array[count] = matrix[i][j] ;
+                count++;
+            }
+        }
+
+        return array;
+    }
+
+
+    private Integer[] rightMove(Integer[] array)
+    {
+        int[][] matrix = convertArray(array);
+        int side = 4;
+        for(int i = 0 ; i < side; i++)
+            matrix = rightMoveOnRow(i, matrix);
+
+        return convertMatrix(matrix);
+
+    }
+
+    private int[][] rightMoveOnRow(int row, int[][] matrix)
+    {
+        int side = 4;
+
+        for (int i = 0 ; i < side - 1; i++)
+        {
+            if(matrix[row][i] == matrix[row][i+1] || matrix[row][i+1] == 0)
+            {
+
+                matrix[row][i+1] += matrix[row][i];
+                score = Math.max(score, matrix[row][i+1]);
+
+                if(i == 0)
+                {
+                    matrix[row][i] = 0;
+                }
+                else
+                {
+                    matrix[row][i] = matrix[row][i-1]; // ar trebui sa se propage in lant.
+                }
+
+            }
+        }
+        return matrix;
+    }
+    private void leftMove(Integer[] array)
+    {
+        int[][] matrix = convertArray(array);
+        for(int i = 0; i < side; i++)
+            matrix = leftMoveOnRow(i,matrix);
+
+
+    }
+
+    private int[][] leftMoveOnRow(int row, int[][] matrix)
+    {
+        int side = 4;
+        for (int j = side - 2; j >=0; j-- )
+        {
+            if(matrix[row][j] == matrix[row][j-1] || matrix[row][j-1] == 0)
+            {
+                matrix[row][j - 1] = matrix[row][j] + matrix[row][j - 1];
+                matrix[row][j] = matrix[row][j-1];
+            }
+        }
+        return matrix;
+    }
+
+    public void changeGrid(Integer[] titls)
+    {
+        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titls);
+        grid.setAdapter(arrayAdapter);
+    }
+{
+
+}
 
 
 }
